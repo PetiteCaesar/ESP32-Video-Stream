7917647fabee6e26e91bdbbfedc533dca45126b2
PetiteCaesar647 <petiteCaesar647@gmail.com>
2026-04-07 02:55:40 +1000

    improve performance

--- src/main.cpp
+++ src/main.cpp
@@ -1,176 +1,268 @@
 #include <Arduino.h>
-#include <TJpg_Decoder.h>
-#include <SPI.h>
+#include <JPEGDEC.h>
 #include <WiFi.h>
-#include <WiFiClient.h>
 #include <WebSocketsServer.h>
-#include <Wire.h>
 #include ".config.h"
 #include <TFT_eSPI.h>
 
-uint16_t w = 0, h = 0;
 
-uint8_t counter = 0;
+#define THREADED
+
 
 uint8_t content[1] = {0};
-
-const uint8_t bufLen = 10;
 bool rec = false;
-bool getSize = true;
+
 WebSocketsServer webSocket = WebSocketsServer(80);
+
 TFT_eSPI tft = TFT_eSPI();
-TFT_eSprite spr = TFT_eSprite(&tft);
-TaskHandle_t Task1;
-TaskHandle_t Task2;
 
-bool display(int16_t x, int16_t y, uint16_t w, uint16_t h, uint16_t *bitmap)
-{
-  if (y >= tft.height())
-    return 0;
-  tft.pushImage(x, y, w, h, bitmap); // takes like 44ms
-  return 1;
-}
+TaskHandle_t drawTask;
+TaskHandle_t networkTask;
 
-bool Sdisplay(int16_t x, int16_t y, uint16_t w, uint16_t h, uint16_t *bitmap)
-{
-  if (y >= tft.height())
-    return 0;
-   spr.pushImage(x, y, w, h, bitmap); // takes like 44ms
-  return 1;
-}
+bool kickedOff = false;
 
-#define tickDelay 1
+JPEGDEC jpeg;
 
-void onWebSocketEvent(uint8_t num, WStype_t type, uint8_t *payload, size_t length)
-{
+struct FrameBuffer{
 
-  switch (type)
-  {
-
-
-  case WStype_DISCONNECTED:
-    Serial.printf("[%u] Disconnected!\n", num);
-    break;
-
-  case WStype_CONNECTED:
-  {
-    IPAddress ip = webSocket.remoteIP(num);
-    Serial.printf("[%u] Connection from ", num);
-    Serial.println(ip.toString());
-  }
-  break;
-
-  case WStype_TEXT:
-    Serial.printf("[%u] Text: %s\n", num, payload);
-    webSocket.sendTXT(num, payload);
-    getSize = true;
-    payload[length] = 0;
-    spr.fillScreen(TFT_BLACK);
-    if (strcmp((char *)payload, "recv") == 0)
-    {
-      rec = true;
+    FrameBuffer()=default;
+    ~FrameBuffer() {
+        if(m_canFree) delete[] m_frameBuf;
     }
-    else if (strcmp((char *)payload, "notrecv") == 0)
-    {
-      rec = false;
+    bool CreateBuf(uint32_t w, uint32_t h){
+        m_width = w; m_height = h;
+        m_size = w * h;
+        m_frameBuf = new uint16_t[m_size];
+        FillBuffer(0);
+        if(!m_frameBuf) return false;
+        m_canFree = true;
+        return true;
     }
 
-    break;
-
-  case WStype_BIN:
-  {
-
-    int t = millis();
-    if(getSize) {
-      TJpgDec.getJpgSize(&w, &h, (const uint8_t *)payload, length);
-      getSize = false;
+    bool CreateBuf(uint8_t* pool, uint32_t w, uint32_t h, bool handleLifetime = false){
+        m_width = w; m_height = h;
+        m_size = w * h;
+        m_frameBuf = (uint16_t*)pool;
+        FillBuffer(0);
+        if(!m_frameBuf) return false;
+        m_canFree = handleLifetime;
+        return true;
     }
-    TJpgDec.drawJpg(0, 0, (const uint8_t *)payload, length);
-    if (rec)
-    {
-      webSocket.sendBIN(num, content, 1);
+
+    void FillBuffer(uint16_t val){
+        if(!m_frameBuf) return;
+        memset(m_frameBuf,val,m_size<<1);
     }
-    int fps = 1000 / ((millis() - t) + 1);
-    Serial.println(fps);
-    Serial.println("fps\n");
-    vTaskDelay(1);
-    break;
-  }
-  case WStype_ERROR:
-  case WStype_FRAGMENT_TEXT_START:
-  case WStype_FRAGMENT_BIN_START:
-  case WStype_FRAGMENT:
-  case WStype_FRAGMENT_FIN:
-  default:
-    break;
-  }
+    void WriteSection(uint32_t x, uint32_t y, uint32_t w, uint32_t h, uint16_t* data){
+        if(!m_frameBuf || !data || x >= m_width || y >= m_height) return;
+        
+        //w + x <= m_width -> w <= x + m_width
+        uint32_t rowCopyLength = min(w, m_width-x);
+        //y + h <= m_height -> h <= m_height - y
+        uint32_t rowsToCopy = min(h,m_height - y);
+
+        uint16_t* start = m_frameBuf + y*m_width + x;
+        while(rowsToCopy--){
+            memcpy(start, data, rowCopyLength<<1);
+            start += m_width;
+            data += w;
+        }
+    }
+
+    void CopyBuffer(const uint8_t* buf, uint32_t size){
+        memcpy(m_frameBuf, buf, size);
+    }
+
+    inline uint32_t GetSizeBytes() const {return (m_width * m_height) << 1;}
+
+    inline const uint16_t* GetBuf()const {return m_frameBuf;}
+
+    private:
+        uint32_t m_width, m_height;
+        uint32_t m_size;
+        uint16_t* m_frameBuf = nullptr;
+        bool m_canFree = false;
+};
+
+FrameBuffer decode;
+FrameBuffer draw1;
+FrameBuffer draw2;
+
+int JPEGDraw(JPEGDRAW *pDraw) {
+    decode.WriteSection(pDraw->x, pDraw->y, pDraw->iWidth, pDraw->iHeight, pDraw->pPixels);
+    return 1;
 }
 
-void mainLoopS(void *pvParameters) {
-  for(;;) {
-    int t = millis();
-    spr.pushSprite(int((320-w)/2), int((170-h)/2), 0, 0,w,h);
-    int fps = 1000 / ((millis() - t) + 1);
-    // vTaskDelay(10);
-    Serial.println(fps);
-    Serial.println("fps Dis\n");
-    vTaskDelay(1); 
-  }
+void onWebSocketEvent(uint8_t num, WStype_t type, uint8_t *payload, size_t length) {
+
+    switch (type) {
+
+        case WStype_DISCONNECTED:
+            Serial.printf("[%u] Disconnected!\n", num);
+        break;
+
+        case WStype_CONNECTED: {
+            IPAddress ip = webSocket.remoteIP(num);
+                Serial.printf("[%u] Connection from ", num);
+                Serial.println(ip.toString());
+            }
+        break;
+
+        case WStype_TEXT:
+            Serial.printf("[%u] Text: %s\n", num, payload);
+            webSocket.sendTXT(num, payload);
+            payload[length] = 0;
+            if (strcmp((char *)payload, "recv") == 0) {
+                rec = true;
+            }
+            else if (strcmp((char *)payload, "notrecv") == 0) {
+                rec = false;
+            }
+
+        break;
+
+        case WStype_BIN: {
+            // Serial.println("Got bin");
+            if(jpeg.openRAM((uint8_t *)payload, length, JPEGDraw)){
+                auto t = millis();
+                if(jpeg.decode(0,0,0)){
+                    // Serial.printf("Decode took: %" PRIu32 "\n",millis()-t);
+                    #ifdef THREADED
+                    if(kickedOff) ulTaskNotifyTake(pdTRUE, portMAX_DELAY); //make sure drawing finished
+                    #endif
+                    //wait for drawing to complete
+                    draw1.CopyBuffer((uint8_t*)decode.GetBuf(), draw1.GetSizeBytes());
+                    draw2.CopyBuffer((uint8_t*)decode.GetBuf()+draw1.GetSizeBytes(), draw2.GetSizeBytes());
+                    // Serial.println("Copied to draw 1 and 2, give noti");
+                    //tell draw task it can start drawing
+                    #ifdef THREADED
+                    xTaskNotifyGive(drawTask);
+                    #else
+                    t = millis();
+                    int draw1Height = (int)TFT_WIDTH/2;
+                    tft.pushImage(0,0,TFT_HEIGHT,draw1Height,draw1.GetBuf());
+                    tft.pushImage(0,draw1Height,TFT_HEIGHT,TFT_WIDTH-draw1Height,draw2.GetBuf());
+                    Serial.printf("memcpy + draw: %" PRIu32 "\n",millis()-t);
+                    #endif
+                }
+                jpeg.close();
+            }
+            webSocket.sendBIN(num, content, 1);
+            //wait to be told it can run
+            // Serial.println("Wait for noti (draw)");
+            
+            // Serial.println("got noti");
+        }
+        break;
+        case WStype_ERROR:
+        case WStype_FRAGMENT_TEXT_START:
+        case WStype_FRAGMENT_BIN_START:
+        case WStype_FRAGMENT:
+        case WStype_FRAGMENT_FIN:
+        default:
+        break;
+    }
 }
 
-void networking(void *pvParameters)
-{
-  Serial.print("Task1 running on core ");
-  Serial.println(xPortGetCoreID());
-  TickType_t xTimeIncrement = pdMS_TO_TICKS(tickDelay);
-  TickType_t xLastWakeTime = xTaskGetTickCount();
-  for (;;)
-  {
+void drawLoop(void* pvParameters) {
+    uint32_t lastDraw = 0;
+    for(;;) {
+        //wait to be asked
+        ulTaskNotifyTake(pdTRUE, portMAX_DELAY);
+        auto s = millis();
+        int draw1Height = (int)TFT_WIDTH/2;
+        tft.pushImage(0,0,TFT_HEIGHT,draw1Height,draw1.GetBuf());
+        tft.pushImage(0,draw1Height,TFT_HEIGHT,TFT_WIDTH-draw1Height,draw2.GetBuf());
+        auto dt = millis()-s;
+        
+        // Serial.printf("Draw buf time: %" PRIu32 " \n",dt); 
+        Serial.printf("Time since lastDraw complete: %" PRIu32 "\n", millis() - lastDraw);
+        lastDraw = millis();
+        xTaskNotifyGive(networkTask);//tell its done
+        kickedOff = true;//for initial "kick off"
+    }
+}
+
+void networkLoop(void* params){
+    for(;;){
+        auto s = millis();
+        webSocket.loop();
+        auto dt = millis()-s;
+        // Serial.printf("webLoop: %" PRIu32 " \n",dt); 
+    }
+}
+
+
+
+void setup() {
+    Serial.begin(115200);
+    delay(1000);
+
+    auto avail1 = heap_caps_get_largest_free_block(MALLOC_CAP_8BIT | MALLOC_CAP_INTERNAL);
+    Serial.print("Avail1 DRAM: ");
+    Serial.println(avail1);
+    if(!decode.CreateBuf(TFT_HEIGHT,TFT_WIDTH)){
+        Serial.println("Failed to create decode buffer");
+    }
+    auto avail2 = heap_caps_get_largest_free_block(MALLOC_CAP_8BIT | MALLOC_CAP_INTERNAL);
+    Serial.print("Avail2 DRAM: ");
+    Serial.println(avail2);
+    int draw1Height = (int)TFT_WIDTH/2;
+    if(!draw1.CreateBuf(TFT_HEIGHT,draw1Height)){
+        Serial.println("Failed to create draw1 buf");
+    }
+    auto avail3 = heap_caps_get_largest_free_block(MALLOC_CAP_8BIT | MALLOC_CAP_INTERNAL);
+    Serial.print("Avail2 DRAM: ");
+    Serial.println(avail3);
+    if(!draw2.CreateBuf(TFT_HEIGHT,TFT_WIDTH-draw1Height)){
+        Serial.println("Failed to create draw2 buf");
+    }
+    
+
+
+    tft.init();
+
+    tft.setRotation(3);
+    tft.setTextColor(0xFFFF, 0x0000);
+    tft.fillScreen(TFT_RED);
+    tft.setSwapBytes(true);
+
+
+
+    Serial.print("Connecting to network: ");
+    Serial.println(SSID);
+
+    WiFi.begin(SSID, PASSWORD);
+    uint8_t counter = 0;
+    while (WiFi.status() != WL_CONNECTED) {
+        delay(500);
+        Serial.print(".");
+        counter++;
+        if (counter >= 60) { // after 30 seconds timeout - reset board
+            esp_restart();
+        }
+    }
+    Serial.println("");
+    Serial.println("WiFi connected");
+    Serial.println("IP address set: ");
+    Serial.println(WiFi.localIP()); // print LAN IP
+
+    tft.fillScreen(TFT_BLACK);
+
+    webSocket.begin();
+    webSocket.onEvent(onWebSocketEvent);
+
+    #ifdef THREADED
+    xTaskCreatePinnedToCore(drawLoop, "DrawTask", 2048, nullptr, 16, &drawTask, 0);
+    xTaskCreatePinnedToCore(networkLoop, "NetworkTask", 8192, nullptr, 16, &networkTask, 1);
+    #endif
+
+  
+}
+void loop(){
+    #ifdef THREADED
+    vTaskDelay(portMAX_DELAY);
+    #else
     webSocket.loop();
-    vTaskDelayUntil(&xLastWakeTime, xTimeIncrement);
-  }
+    #endif
 }
-
-void setup()
-{
-  Serial.begin(115200);
-  tft.begin();
-
-  tft.setRotation(3);
-  tft.setTextColor(0xFFFF, 0x0000);
-  tft.fillScreen(TFT_RED);
-  tft.setSwapBytes(true);
-  spr.createSprite(302, 170);
-  spr.setSwapBytes(true);
-  TJpgDec.setJpgScale(1);
-  TJpgDec.setCallback(Sdisplay);
-  // Connect to access point
-  Serial.println();
-  Serial.print("Connecting to network: ");
-  Serial.println(SSID);
-
-  WiFi.begin(SSID, PASSWORD);
-  while (WiFi.status() != WL_CONNECTED)
-  {
-    delay(500);
-    Serial.print(".");
-    counter++;
-    if (counter >= 60)
-    { // after 30 seconds timeout - reset board
-      ESP.restart();
-    }
-  }
-  Serial.println("");
-  Serial.println("WiFi connected");
-  Serial.println("IP address set: ");
-  Serial.println(WiFi.localIP()); // print LAN IP
-
-  tft.fillScreen(TFT_BLACK);
-  xTaskCreatePinnedToCore(mainLoopS, "Task1", 10000, NULL, 1, &Task2, 1);
-  delay(500);
-  xTaskCreatePinnedToCore(networking, "Task2", 10000, NULL, 1, &Task1, 0);
-  delay(500);
-  webSocket.begin();
-  webSocket.onEvent(onWebSocketEvent);
-}
-void loop(){}
--- src/stream.py
+++ src/stream.py
@@ -7,7 +7,7 @@ import sys
 import websocket
 import time
 
-print("starting")
+print("Starting")
 
 #monitor = {"top": 0, "left": 0, "width": 1920, "height": 1080}
 monitor = {"top": 200, "left": 100, "width": 1280, "height": 720}
@@ -17,9 +17,6 @@ def capture_Screen():
         with mss() as sct:
             return cv2.cvtColor(np.array(sct.grab(monitor)), cv2.COLOR_BGR2RGB)
             
-
-
-
 def capture_Region(x,y,w,h):
     # Capture entire screen
     with mss() as sct:
@@ -30,30 +27,17 @@ def capture_Region(x,y,w,h):
 
 
 #local ip of esp32 
-ip = ""
+ip = "192.168.0.143"
+quality = 80
+rec = True
+verbosity = 10
 i = 0
-s = 0
-quality = 40
 ws = websocket.WebSocket()
 ws.connect("ws://"+ip)
 print("Connected to WebSocket server")
-dis = 0
-rec = True
-aspect = 16
-noCompQ = quality
-compA = False 
-comp = True
 
-uf = 10
-
-compThresh = 6000 #normal 3400
-ft = 0
-
-
-
-ws.send("dis"+str(dis))
 if rec:
-    ws.send("recv") #reciveing
+    ws.send("recv") #Should send back ack
 else:
     ws.send("notrecv")
 
@@ -63,101 +47,24 @@ while True:
 
 
             stn = time.time()
-            if aspect == 16:
-                #img = ImageGrab.grab(bbox=(0, 0, 1920, 1080)) #16:9
-                img = capture_Screen()
-                #img = capture_Region(400,600,240,240)
-                #img = ImageGrab.grab(bbox=None,include_layered_windows=False,all_screens=False,xdisplay=None)
-            elif aspect == 4:
-                img = capture_Region(240, 0, 1680, 1080) #4:3
-            elif aspect == 5:
-                img = capture_Region(240, 0, 1440, 1080) #idk 💀
-            elif aspect == 6:
-                img = capture_Region(285, 0, 1350, 1080) #idk 💀
-            else:
-                img = capture_Region(54, 0, 800, 640) #16:9
+            img = capture_Screen()
 
-            capt = time.time()-stn
-
-
-
-            img_np = np.array(img)
-
-            frame = cv2.cvtColor(img_np, cv2.COLOR_BGR2RGB)
-            #frame = cv2.cvtColor(img_np, cv2.COLOR_BGR2G   RAY)
-            if dis == 0:
-                if aspect == 16:
-                    #final = cv2.resize(frame,(120,64))
-                    #final = cv2.resize(frame,(160,90)) #16:9
-                    #final = cv2.resize(frame,(240,128)) #16:9
-                    #final = cv2.resize(frame,(480,270))
-                    #final = cv2.resize(frame,(354,200))
-                    final = cv2.resize(frame,(302,170))
-                else:
-                    #final = cv2.resize(frame,(160,128))
-                    final = cv2.resize(frame,(240,240))
-            else:
-                final = cv2.resize(frame,(128,64))
-            
-
-            jpeg = cv2.imencode('.jpg', final,[int(cv2.IMWRITE_JPEG_QUALITY), quality])[1].tobytes()
-
-            if compA:
-                if comp:
-                    if sys.getsizeof(jpeg) > compThresh:
-                        #print("Frame size: ", sys.getsizeof(jpeg))
-                        q = sys.getsizeof(jpeg) - compThresh
-                        d = quality-int(q / 100)
-                        
-                        if d >= quality:
-                            d = 10
-                        #print(d)
-                        jpeg = cv2.imencode('.jpg', final,[int(cv2.IMWRITE_JPEG_QUALITY), d])[1].tobytes()
-                        if i % uf==0:
-                            #print("Frame size after: ", sys.getsizeof(jpeg),d)
-                            pass
-                else:
-                    s = sys.getsizeof(jpeg)
-                    if s > compThresh:
-                        d = int((noCompQ-(s/1000))) if int((noCompQ-(s/1000))) < quality else quality
-                        print(d)
-                        jpeg = cv2.imencode('.jpg', final,[int(cv2.IMWRITE_JPEG_QUALITY), d])[1].tobytes()
-            else:
-                if sys.getsizeof(jpeg) > 12000:
-                    jpeg = cv2.imencode('.jpg', final,[int(cv2.IMWRITE_JPEG_QUALITY), 10])[1].tobytes()
+            frame = cv2.cvtColor(np.array(img), cv2.COLOR_BGR2RGB)
+            final = cv2.resize(frame,(240,135))
+            jpeg = cv2.imencode('.jpg', final, [int(cv2.IMWRITE_JPEG_QUALITY), quality])[1].tobytes()
 
             ws.send_binary(jpeg)
-            
-            
-            ft = round(time.time()-stn,4)
-
-            if i % uf==0:
-                    print(ft,"BFPS:",round((1/(time.time()-stn)),2))
-                    print("Frame size: ", sys.getsizeof(jpeg))
-            if not rec:
-                time.sleep(0.008)
-                pass
-            else:
-                #result = ws.recv()
+                    
+            if rec:
                 result = ws.recv_frame()
-                #print(result)
-                # if i % 5==0:
-                #     print("Recv FPS:",time.time()-cap)
-
-
-            # if capt > 30:
-            #     ttn = time.time()
-            #     while True:
-            #         current_time = time.time()
-            #         elapsed_time = current_time - ttn
-            #         if elapsed_time > ((1/30)-ttn+stn):
-            #             break
-
+                
             fps = round(1/(time.time()-stn),2)
-            if i % uf==0:
-                print("FPS:",fps,"\n")
+            if i % verbosity==0:
+                print("\nFPS:",fps)
+                print("Frame size: ", sys.getsizeof(jpeg))
                 i = 0
             i+=1
+            
         except KeyboardInterrupt:
             print("KeyboardInterrupt, Attempting to close (please work)")
             ws.close()
